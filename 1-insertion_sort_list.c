1ba88b02bbed40296a88577294d3b18edb439ece
demystik <demystiktech@gmail.com>
2022-10-07 21:00:54 +0100

    1 insertion added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,16 +3,6 @@
 void insertion_sort_list(listint_t **list)
 {
 
-    listint_t *head, *temp, *h;
-    head = *list;
-    temp->prev->next = h->prev;
-    temp->next = h->next->prev;
-    h->next->prev = temp->next;
-    h->prev = temp->prev->next;
-    h->next = temp->prev;
-    temp->prev = h->next;
-    
-    /*
     listint_t *head, *i, *h, *temp;
     i = head = *list;
 
@@ -20,23 +10,45 @@ void insertion_sort_list(listint_t **list)
     while (i != NULL)
     {
         h = i;
-        while(h != NULL && h->prev->n > h->n)
+        while(h->prev != NULL && h->prev->n > h->n)
         {
             temp = h->prev;
-            //swap
-            temp->prev->next = h->prev;
-            temp->next = h->next->prev;
-            h->next->prev = temp->next;
-            h->prev = temp->prev->next;
-            h->next = temp->prev;
-            temp->prev = h->next;
-            // done swapping
+            /* swap */
+            if (temp->prev == NULL)
+            {
+            temp->next = h->next;
+            h->next->prev = temp;
+            h->prev = NULL;
+            h->next = temp;
+            temp->prev = h;
+            }
+            else if (h->next == NULL)
+            {
+                temp->prev->next = h;
+                h->next = temp;
+                h->prev = temp->prev;
+                temp->prev = h;
+                temp->next = NULL;
+            }
+            else
+            {
+            temp->prev->next = h;
+            temp->next = h->next;
+            h->next->prev = temp;
+            h->prev = temp->prev;
+            h->next = temp;
+            temp->prev = h;
+            }
+            /* done swapping */
+            while(head->prev != NULL)
+                head = head->prev;
+            *list = head;
+            print_list(*list);
 
-            h = h->prev;
+           
         }
-        print_list(*list);
         i = i->next;
     }
-    */
 
 }
+
