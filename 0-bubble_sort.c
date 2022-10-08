134fdd6eba8d35b84cd69d23bdf2741ad761cf83
demystik <demystiktech@gmail.com>
2022-10-08 20:50:52 +0100

    task zero updated :

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,31 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - This function sort an array of integers
+ * using bubble method
+ *@array: array of unsorted integers
+ *@size: size of the array
+ *Author: Thaoban Abdrasheed
+ *
+ * Return: nothing to return
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, j;
+int temp;
+size_t i, j;
 
-    for (i = 0; i != size; i++)
-    {
-        for(j = 1; j != size; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+for (i = 0; i != size; ++)
+{
+for (j = 1; j != size; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
