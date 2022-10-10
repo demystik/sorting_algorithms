b91b8169742a4a894e85f54ae562b55912ff33e7
demystik <demystiktech@gmail.com>
2022-10-10 14:03:05 +0100

    quicksort updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,37 +1,57 @@
 #include "sort.h"
 
-void quicksort(int *array,int first,int last)
+
+/**
+ * quicksort - Implementation fo selection sorting
+ *@first: index of the first num
+ *@last: index of the last number
+ *Author: Thaoban Abdrasheed
+ *
+ *Return: nothing to return
+ */
+void quicksort(int *array, int first, int last)
 {
    int i, j, pivot, temp, reserve;
 
    reserve = last + 1;
-   if(first < last)
+   if (first < last)
    {
-      pivot=last;
-      i=first;
-      j=last;
-      while(i < j)
+      pivot = last;
+      i = first;
+      j = last;
+      while (i < j)
       {
-         while(array[i] <= array[pivot] && i < last)
+         while (array[i] <= array[pivot] && i < last)
             i++;
-         while(array[j]>array[pivot])
+         while (array[j] > array[pivot])
             j--;
-         if(i<j){
-            temp=array[i];
-            array[i]=array[j];
-            array[j]=temp;
+         if (i < j)
+         {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
             print_array(array, reserve);
          }
       }
-      temp=array[pivot];
-      array[pivot]=array[j];
-      array[j]=temp;
-      quicksort(array,first,j-1);
-      quicksort(array,j+1,last);
+      temp = array[pivot];
+      array[pivot] = array[j];
+      array[j] = temp;
+      quicksort(array, first, j - 1);
+      quicksort(array, j + 1, last);
    }
 }
+
+/**
+ * quick_sort - Implementation of quick-sorting algo
+ *@array: pointer to the array of integers
+ *@size: size of the array
+ *Author: Thaoban Abdrasheed
+ *
+ *Return: nothing to return
+ */
+
 void quick_sort(int *array, size_t size)
 {
-  
-    quicksort(array, 0, size - 1);
+   quicksort(array, 0, size - 1);
 }
+
