bc0d32ffdfd2fe12edb6b5274e98f93422c84e09
demystik <demystiktech@gmail.com>
2022-10-10 14:02:31 +0100

    header file updated

--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
+
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,16 +14,19 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif /*SORT_H*/
+#endif /* SORT_H */
