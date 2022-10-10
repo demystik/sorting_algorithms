276bf758314c0c81f2bcba8163e56e8a8221ac06
demystik <demystiktech@gmail.com>
2022-10-10 11:30:35 +0100

    bubble sort task updatedd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,8 +14,11 @@ void bubble_sort(int *array, size_t size)
 {
 int temp;
 size_t i, j;
-if (size > 1)
+
+if (array == NULL || size < 2)
 {
+return;
+}
 for (i = 0; i != size; i++)
 {
 for (j = 0; j != size - 1; j++)
@@ -26,7 +29,6 @@ temp = array[j + 1];
 array[j + 1] = array[j];
 array[j] = temp;
 print_array(array, size);
-}
 }
 }
 }
