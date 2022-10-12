81b2ebdfa58ffbf5b50e745ae579e8e42e92f53e
demystik <demystiktech@gmail.com>
2022-10-12 14:21:44 +0100

    test_addedd

--- test.c
+++ test.c
@@ -37,24 +37,6 @@ int partition(int *arr, int low, int high)
 	return (i + 1);
 }
 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int *arr, int low, int high)
-{
-	if (low < high)
-	{
-		/* pi is partitioning index, arr[p] is now
-		at right place */
-		int pi = partition(arr, low, high);
-
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
-}
 
 
 
