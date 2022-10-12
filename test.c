774a0a71051031ea9fc06104e8a29ab83c6e35ff
demystik <demystiktech@gmail.com>
2022-10-12 14:22:12 +0100

    test_addedd

--- test.c
+++ test.c
@@ -17,17 +17,7 @@ int partition(int *arr, int low, int high)
 
 	for (j = low; j <= high- 1; j++)
 	{
-		// If current element is smaller than or
-		// equal to pivot
-		if (arr[j] <= pivot)
-		{
-			i++; // increment index of smaller element
-            //swap
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            //printArray(arr, high + 1);
-        }
+		
 	}
    
  
