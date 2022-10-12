71fae28425ec64a4a7364688ff9d0ef5c634f6c7
demystik <demystiktech@gmail.com>
2022-10-12 14:21:21 +0100

    test_added

--- test.c
+++ test.c
@@ -56,14 +56,6 @@ void quickSort(int *arr, int low, int high)
 	}
 }
 
-/* Function to print an array */
-void printArray(int *arr, int size)
-{
-	int i;
-	for (i=0; i < size; i++)
-	printf("%d ", arr[i]);
-	printf("\n");
-}
 
 
 // Driver program to test above functions
